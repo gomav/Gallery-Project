f37a9d83c2df4cd8e8ece866994ea5f3379b1118
Manny Navarro <manny.navarro@gmail.com>
2018-09-05 13:25:07 -0700

    adding vanilla Js overlay, modifying screen size for the overlay

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,32 @@
-console.log('it works')
+(function(){
+"use strict";
+    var carimages = document.querySelector('.carimages');
+
+    carimages.addEventListener('click', function(e){
+        if(e.target.tagName === 'IMG'){
+
+            var overlay = document.createElement('div');
+            overlay.id = 'overlay';
+
+            //Set some styles for the overlay
+            overlay.style.position = 'absolute';
+            overlay.style.top = 0;
+            overlay.style.background = 'rgba(0,0,0,0.7)';
+            overlay.style.cursor = 'pointer';
+
+            //Set some sizes for the overlay
+
+            overlay.style.width = window.innerWidth + "px";
+            overlay.style.height = window.innerHeight + "px";
+
+            document.body.appendChild(overlay);
+
+            console.log(e.target.tagName);
+
+        }
+
+    });
+
+
+
+}());
